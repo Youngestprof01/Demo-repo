b2462a3f4c95c398ca81bcd5e6500bd7ae80eb5d
Ganiyu Idris Abimbola <107248016+Youngestprof01@users.noreply.github.com>
2023-03-09 13:05:42 +0000

    changes

---
+++ process.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import tui
+
+def add():
+    num_1 = tui.num_1()
+    num_2 = tui.num_2()
+    add = num_1 + num_2
+    return add
+
+
+# In[ ]:
+
+
+
+
---
+++ tui.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def welcome():
+    print('-----------------------------------------')
+    print('Welcome to my project')
+    print('------------------------------------------')
+    
+def num_1():
+     num_1 = int(input('Enter Number 1: '))
+    return num_1
+def num_2():
+    num_2 = int(input('Enter Number 2: '))
+    return num_2
+
